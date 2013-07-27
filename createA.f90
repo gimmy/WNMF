3f9cd47afd5bd9b910f69dd5710fba471e1951c0
Gianmarco Brocchi <brocchi@poisson.phc.unipi.it>
2013-07-27 12:54:57 +0200

    Corretta creazione di A

--- createA.f90
+++ createA.f90
@@ -1,12 +1,21 @@
 SUBROUTINE createA ()
   ! Crea la matrice A le cui colonne sono i vector face
-  INTEGER n                     ! indice per le immagini
+  ! le immagini sono 92x112 = 10304
+  INTEGER n, m                     ! indice per le immagini
   INTEGER ( kind = 4 ) :: file_unit
   CHARACTER ( len=1024 ) :: filename
   CHARACTER ( len=1024 ) :: format_string
 
-  INTEGER :: row, col = 10                    ! righe e colonne di A (numero di immagini)
-  INTEGER ( kind = 4 ), ALLOCATABLE, DIMENSION ( :, : ) :: g ! matrice A
+  INTEGER ( kind = 4 ) i,j,k
+  INTEGER ( kind = 4 ) ierror
+  INTEGER ( kind = 4 ) ios
+  INTEGER ( kind = 4 ) nrow, ncol, maxg
+
+  INTEGER :: row = 10304, col = 10                    ! righe e colonne (numero di immagini) di A
+  INTEGER ( kind = 4 ), ALLOCATABLE, DIMENSION ( :, : ) :: readPGM, A 
+
+  ALLOCATE ( A(row,col) )
+  WRITE(*,*) 'Allocata matrice A: ', SHAPE(A)
 
   DO n=1,col
      IF (n < 10) then
@@ -19,7 +28,7 @@ SUBROUTINE createA ()
      ! PRINT *, trim(filename)
 
      CALL get_unit ( file_unit )
-
+     ! Apro il file PGM
      OPEN ( UNIT=file_unit, FILE=filename, STATUS='old', IOSTAT=ios )
      IF ( ios /= 0 ) THEN
         WRITE ( *, '(a)' ) ' '
@@ -29,71 +38,35 @@ SUBROUTINE createA ()
      ENDIF
 
      CALL pgma_read_header ( file_unit, nrow, ncol, maxg )
-
      ! WRITE ( *, '(a)' ) ' '
-     ! WRITE ( *, '(a,a12)' ) '  PGMA_READ_HEADER read the header.', filename
+     ! WRITE ( *, '(a,a12)' ) '  PGMA_READ_HEADER read the header. ', filename
      ! WRITE ( *, '(a)' ) ' '
      ! WRITE ( *, '(a,i8)' ) '  Number of rows of data =    ', nrow
      ! WRITE ( *, '(a,i8)' ) '  Number of columns of data = ', ncol
      ! WRITE ( *, '(a,i8)' ) '  Maximum G value =           ', maxg
 
+     ! Lettura dati
+     ALLOCATE ( readPGM(nrow,ncol) )
+     ! WRITE(*,*) 'Alloco matrice readPGM per lettura dati: ', SHAPE(readPGM)
+     CALL pgma_read_data ( file_unit, nrow, ncol, readPGM )
+
+     ! Salvo g come vettore in A
+     m = 1
+     DO i = 1, nrow
+        DO j = 1, ncol
+           A(m,n) = readPGM(i,j)
+           m = m+1
+        END DO
+     END DO
+     
+     DEALLOCATE (readPGM)
+
   END DO
 
-  row = row_num * col_num
-  ALLOCATE ( g(row,col) )
+  WRITE(*,*) ' Scritti vector face in A'! , SHAPE(A)
 
-  CALL read_data ( file_unit, row, col, g )
-
-  DEALLOCATE ( g )
+  DEALLOCATE (A)
 
   RETURN
 
 END SUBROUTINE createA
-
-
-SUBROUTINE read_data ( file_in_unit, row_num, col_num, A )
-
-!********************************************************************
-!
-!! PGMA_READ_DATA reads the data in an ASCII PGM file.
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license. 
-
-  IMPLICIT NONE
-
-  INTEGER  col_num
-  INTEGER  row_num
-
-  LOGICAL done
-  INTEGER ( kind = 4 ) file_in_unit
-  INTEGER ( kind = 4 ) A(row_num,col_num)
-  INTEGER ( kind = 4 ) i
-  INTEGER ( kind = 4 ) ierror
-  INTEGER ( kind = 4 ) j
-  CHARACTER ( len = 80 ) string
-
-  ierror = 0
-  done = .TRUE.
-  string = ' '
-
-  DO i = 1, row_num
-     DO j = 1, col_num
-
-        CALL getint ( done, ierror, file_in_unit, A(i,j), string )
-
-        IF ( ierror /= 0 ) THEN
-           CLOSE ( unit = file_in_unit )
-           WRITE ( *, '(a)' ) ' '
-           WRITE ( *, '(a)' ) 'PGMA_READ_DATA - Fatal error!'
-           WRITE ( *, '(a)' ) '  Problem reading G data.'
-           ! STOP
-        END IF
-
-     END DO
-  END DO
-
-  RETURN
-
-END SUBROUTINE read_data
