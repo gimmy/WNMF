5195ab322d8c3224c1e5ab7e487ecad5d7ac822b
Gianmarco Brocchi <brocchi@poisson.phc.unipi.it>
2013-08-03 21:27:39 +0200

    Calcola la matrice V

--- createV.f90
+++ createV.f90
@@ -6,38 +6,56 @@ SUBROUTINE createV(A,U,V, row, rank, col)
   REAL, DIMENSION(rank,col) :: V
   INTEGER i, j
 
-  ! REAL, DIMENSION(rank,col) :: Va
-  ! REAL, DIMENSION(rank,rank) :: Uk, invUk
-  ! REAL, DIMENSION(rank) :: y
-
-  CALL RANDOM_NUMBER(V)
+  REAL, DIMENSION(rank,col) :: Va
+  REAL, DIMENSION(rank,rank) :: Uk, invUk
+  REAL, DIMENSION(rank) :: y
 
   ! Salvo la sottomatrice U_k
+  DO i = 1, rank
+     DO j = 1, rank
+        Uk(i,j) = U(i,j)
+     END DO
+  END DO
+
+  CALL FINDinv(Uk,invUk, rank, error)
+  ! invUk = inv(Uk)
+  ! CALL inv(Uk,invUk)
+  WRITE(*,*) 'inversa di Uk calcolata'
+
+  ! ! Controllo NonNegatività di invUk
   ! DO i = 1, rank
   !    DO j = 1, rank
-  !       Uk(i,j) = U(i,j)
+  !       IF ( invUk(i,j) <= 0 ) THEN
+  !          STOP 'trovato elemento <= 0 in invUk'
+  !       ENDIF
   !    END DO
   ! END DO
 
-  ! CALL FINDinv(Uk,invUk, rank, error)
-  ! WRITE(*,*) 'invertita Uk'
+  DO j=1,col
+     ! Calcolo y
+     DO i=1,rank
+        y(i) = A(i,j)
+     ENDDO
+     ! WRITE(*,*) 'MATMUL...'
+     ! WRITE(*,*) SHAPE(V(:,j)), SHAPE(invUk), SHAPE(y)
+     V(:,j)= MATMUL(invUk, y)
+  END DO
 
-  ! DO j=1,col
-  !    ! WRITE(*,*) 'Calcolo y'
-  !    y = A(:,j)
-  !    ! WRITE(*,*) 'MATMUL...'
-  !    V(:,j)= MATMUL(invUk, y)
-  !    ! WRITE(*,*) SHAPE(V(:,j)), SHAPE(y), SHAPE( A(:,j) )
-  ! END DO
-
-  ! Controllo NonNegatività di U
-  DO i = 1, row
-     DO j = 1, rank
-        IF ( U(i,j) <= 0 ) THEN
-           ! U(i,j) = 0
-           STOP 'trovato elemento <= 0'
+  ! Controllo NonNegatività di V
+  DO i = 1, rank
+     DO j = 1, col
+        IF ( V(i,j) < 0 ) THEN
+           ! STOP 'trovato elemento < 0 in V'
+           ! write(*,*) 'trovato elemento < 0 in V, lo metto a 0'
+           V(i,j) = 0
+        ENDIF
+        IF ( V(i,j) > 0 ) THEN
+           ! STOP 'trovato elemento > 0 in V'
+           k = k+1
         ENDIF
      END DO
   END DO
+
+  write(*,'(A,I5,A,I5)') 'Non distruggerò V per ',k,' elementi giusti su ',rank*col
 
 END SUBROUTINE createV
